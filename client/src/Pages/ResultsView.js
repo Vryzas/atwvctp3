50533cc18753d769279e2dc8a1a2deb21f807719
Vryzas <vryzas27@gmail.com>
2022-05-08 16:54:12 +0100

    adrressing

--- client/src/Pages/ResultsView.js
+++ client/src/Pages/ResultsView.js
@@ -1,125 +1,125 @@
-import {Alert, Button, Col, Form, Row} from "react-bootstrap";
-import React, {useEffect, useState} from "react";
-import {useHistory, useParams} from "react-router-dom";
-import Container from "react-bootstrap/Container";
-import Axios from "axios";
-import Table from "react-bootstrap/Table";
-
-
+import { Alert, Button, Col, Form, Row } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Axios from 'axios';
+import Table from 'react-bootstrap/Table';
 
 function ResultsView() {
+  let history = useHistory();
+  let id = useParams();
 
-    let history = useHistory();
-    let id = useParams();
+  const [unvoted, setUnvoted] = useState(0);
+  useEffect(() => {
+    Axios.get(`https://atwvc.herokuapp.com/getnovotes/${id.id}`).then(
+      (response) => {
+        setUnvoted(response.data[0].total);
+      }
+    );
+  }, []);
 
-    const [unvoted, setUnvoted] = useState(0);
-    useEffect( () => {
-        Axios.get(`http://localhost:3001/getnovotes/${id.id}`).then((response) => {
-            setUnvoted(response.data[0].total);
-        })
-    },[]);
+  const [publicvoters, setPublicVoters] = useState([]);
+  useEffect(() => {
+    Axios.get(`https://atwvc.herokuapp.com/getpublic/${id.id}`).then(
+      (response) => {
+        setPublicVoters(response.data);
+      }
+    );
+  }, []);
 
-    const [publicvoters, setPublicVoters] = useState([]);
-    useEffect( () => {
-        Axios.get(`http://localhost:3001/getpublic/${id.id}`).then((response) => {
-            setPublicVoters(response.data);
-        })
-    },[]);
+  const [votes, setVotes] = useState([]);
+  useEffect(() => {
+    Axios.get(`https://atwvc.herokuapp.com/getvotes/${id.id}`).then(
+      (response) => {
+        setVotes(response.data);
+      }
+    );
+  }, []);
 
-    const [votes, setVotes] = useState([]);
-    useEffect( () => {
-        Axios.get(`http://localhost:3001/getvotes/${id.id}`).then((response) => {
-            setVotes(response.data);
-        })
-    },[]);
-
-    let total = unvoted;
-    const totalvotes = {votes}.votes.map((item) => {
-        total += item.countvotos;
-    })
-    const votesLists = {votes}.votes.map((item) => {
-        const perc = (item.countvotos/total)*100;
-        return (
-            <tr>
-                <td>{item.listanome}</td>
-                <td>{item.countvotos}</td>
-                <td>{Math.round(perc)+"%"}</td>
-            </tr>
-        )
-    });
-
-    const publicVoters = {publicvoters}.publicvoters.map((item) => {
-        return (
-            <tr>
-                <td>{item.nome}</td>
-                <td>{item.descricao}</td>
-            </tr>
-        )
-    });
-
+  let total = unvoted;
+  const totalvotes = { votes }.votes.map((item) => {
+    total += item.countvotos;
+  });
+  const votesLists = { votes }.votes.map((item) => {
+    const perc = (item.countvotos / total) * 100;
     return (
-        <Container fluid>
-            <Row className="header">
-                <h2>Resultados</h2>
-                <h4>Bem vindo {sessionStorage.getItem("name")}</h4>
-            </Row>
-            <header> <h1> Resultados </h1> </header>
-            <Button
-                variant="secondary"
-                className="button"
-                onClick={history.goBack}>
-                Voltar atrás
-            </Button>
-            <hr/>
+      <tr>
+        <td>{item.listanome}</td>
+        <td>{item.countvotos}</td>
+        <td>{Math.round(perc) + '%'}</td>
+      </tr>
+    );
+  });
 
-            <Row className="resultados">
-                <Col xs={3}>
-                    <h3>Votos por Lista</h3>
-                    <Table responsive>
-                    <thead>
-                    <tr>
-                        <th scope="col">Designação</th>
-                        <th scope="col">Votos</th>
-                        <th scope="col">Percentagem</th>
-                        <th scope="col"></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {votesLists}
-                    <tr>
-                        <td>Abstenção</td>
-                        <td>{unvoted}</td>
-                        <td>{Math.round((unvoted/total)*100)+"%"}</td>
-                    </tr>
-                    </tbody>
-                </Table>
-                </Col>
-                <Col xs={3}>
-                    <tr>
-                        <th scope="col">Gráfico</th>
-                    </tr>
-                </Col>
-                <Col xs={3}>
-                    <h3>Votos Públicos</h3>
-                    <Table responsive>
-                        <thead>
-                        <tr>
-                            <th scope="col">Votante</th>
-                            <th scope="col">Lista Votada</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {publicVoters}
-                        </tbody>
-                    </Table>
-                </Col>
+  const publicVoters = { publicvoters }.publicvoters.map((item) => {
+    return (
+      <tr>
+        <td>{item.nome}</td>
+        <td>{item.descricao}</td>
+      </tr>
+    );
+  });
 
-            </Row>
-            <Row className="rodape2" >
-                <p>Votrónico+ | Vitor Costa 2021</p>
-            </Row>
-        </Container>
-    )
+  return (
+    <Container fluid>
+      <Row className="header">
+        <h2>Resultados</h2>
+        <h4>Bem vindo {sessionStorage.getItem('name')}</h4>
+      </Row>
+      <header>
+        {' '}
+        <h1> Resultados </h1>{' '}
+      </header>
+      <Button variant="secondary" className="button" onClick={history.goBack}>
+        Voltar atrás
+      </Button>
+      <hr />
+
+      <Row className="resultados">
+        <Col xs={3}>
+          <h3>Votos por Lista</h3>
+          <Table responsive>
+            <thead>
+              <tr>
+                <th scope="col">Designação</th>
+                <th scope="col">Votos</th>
+                <th scope="col">Percentagem</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {votesLists}
+              <tr>
+                <td>Abstenção</td>
+                <td>{unvoted}</td>
+                <td>{Math.round((unvoted / total) * 100) + '%'}</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Col>
+        <Col xs={3}>
+          <tr>
+            <th scope="col">Gráfico</th>
+          </tr>
+        </Col>
+        <Col xs={3}>
+          <h3>Votos Públicos</h3>
+          <Table responsive>
+            <thead>
+              <tr>
+                <th scope="col">Votante</th>
+                <th scope="col">Lista Votada</th>
+              </tr>
+            </thead>
+            <tbody>{publicVoters}</tbody>
+          </Table>
+        </Col>
+      </Row>
+      <Row className="rodape2">
+        <p>Votrónico+ | Vitor Costa 2021</p>
+      </Row>
+    </Container>
+  );
 }
 
 export default ResultsView;
--- client/src/Pages/votingOption.js
+++ client/src/Pages/votingOption.js
@@ -1,122 +1,130 @@
-import {Alert, Button, Col, Form, Row, ToggleButton} from "react-bootstrap";
-import React, {useEffect, useState} from "react";
-import {useHistory, useParams} from "react-router-dom";
-import Axios from "axios";
-import Table from "react-bootstrap/Table";
-import Container from "react-bootstrap/Container";
-
-
+import { Alert, Button, Col, Form, Row, ToggleButton } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import Axios from 'axios';
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
 
 function VotingOption() {
+  let history = useHistory();
+  let id = useParams();
+  const userid = sessionStorage.getItem('id');
+  const [msg, setMsg] = useState();
 
-    let history = useHistory();
-    let id = useParams();
-    const userid = sessionStorage.getItem("id");
-    const [msg, setMsg] = useState();
-
-    const submitvote = (idlista) => {
-        let anonimo = 1;
-        if(checked){
-            anonimo = 0;
-        }
-        Axios.post(`http://localhost:3001/vote`, {
-            userid: userid,
-            idele: id.id,
-            listid: idlista,
-            anonim: anonimo,
-        }).then((response) => {
-            if (response.data.affectedRows === 1) {
-                setMsg(<Alert variant="success">
-                    <Alert.Heading>Voto submetido com sucesso!</Alert.Heading></Alert>);
-                    history.goBack();
-            } else {
-                setMsg(<Alert variant="danger">
-                    <Alert.Heading>Falha na votação!</Alert.Heading></Alert>);
-            }
-        });
-    };
-
-    const [lists, setLists] = useState([]);
-    useEffect( () => {
-        Axios.get(`http://localhost:3001/getlists/${id.id}`).then((response) => {
-            setLists(response.data);
-        })
-    },[]);
-
-    const [checked, setChecked] = useState(false);
-
-    const votterlist = {lists}.lists.map((item) => {
-        return (
-            <tr value={item.id_listas}>
-                <td>{item.id_listas}</td>
-                <td>{item.descricao}</td>
-                <td>{item.nome_can}</td>
-                <td>
-                    <Button
-                        variant="primary"
-                        className="button"
-                        onClick={submitvote.bind(this,item.id_listas)}>
-                        Submeter voto
-                    </Button>
-                </td>
-            </tr>
-        )
+  const submitvote = (idlista) => {
+    let anonimo = 1;
+    if (checked) {
+      anonimo = 0;
+    }
+    Axios.post(`https://atwvc.herokuapp.com/vote`, {
+      userid: userid,
+      idele: id.id,
+      listid: idlista,
+      anonim: anonimo,
+    }).then((response) => {
+      if (response.data.affectedRows === 1) {
+        setMsg(
+          <Alert variant="success">
+            <Alert.Heading>Voto submetido com sucesso!</Alert.Heading>
+          </Alert>
+        );
+        history.goBack();
+      } else {
+        setMsg(
+          <Alert variant="danger">
+            <Alert.Heading>Falha na votação!</Alert.Heading>
+          </Alert>
+        );
+      }
     });
+  };
 
+  const [lists, setLists] = useState([]);
+  useEffect(() => {
+    Axios.get(`https://atwvc.herokuapp.com/getlists/${id.id}`).then(
+      (response) => {
+        setLists(response.data);
+      }
+    );
+  }, []);
+
+  const [checked, setChecked] = useState(false);
+
+  const votterlist = { lists }.lists.map((item) => {
     return (
-        <Container fluid>
-            <Row className="header">
-                <h2>Voto</h2>
-                <h4>Bem vindo {sessionStorage.getItem("name")}</h4>
-            </Row>
-            <header> <h1> Votação </h1> </header>
-            {msg}
-            <Row className="votos">
-                <Form className="votos">
-                    <Table responsive>
-                        <thead>
-                        <tr>
-                            <th scope="col"> # </th>
-                            <th scope="col"> Descrição </th>
-                            <th scope="col"> Nome Candidato </th>
-                            <th scope="col"> Voto </th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {votterlist}
-                        </tbody>
-                    </Table>
-                    <Row>
-                        <Col></Col>
-                        <Col>
-                            <ToggleButton
-                                className="mb-2"
-                                id="toggle-check"
-                                type="checkbox"
-                                variant="outline-primary"
-                                checked={checked}
-                                value="1"
-                                onChange={(e) => setChecked(e.currentTarget.checked)}
-                            >
-                                Voto Público
-                            </ToggleButton>
-                        </Col>
-                        <Col>
-                            <Button
-                                variant="secondary"
-                                className="button"
-                                onClick={history.goBack}>
-                                Voltar atrás
-                            </Button>
-                        </Col>
-                    </Row>
-                </Form>
-            </Row>
-            <Row className="rodape2" >
-                <p>Votrónico+ | Vitor Costa 2021</p>
-            </Row>
-        </Container>
-    )
+      <tr value={item.id_listas}>
+        <td>{item.id_listas}</td>
+        <td>{item.descricao}</td>
+        <td>{item.nome_can}</td>
+        <td>
+          <Button
+            variant="primary"
+            className="button"
+            onClick={submitvote.bind(this, item.id_listas)}
+          >
+            Submeter voto
+          </Button>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <Container fluid>
+      <Row className="header">
+        <h2>Voto</h2>
+        <h4>Bem vindo {sessionStorage.getItem('name')}</h4>
+      </Row>
+      <header>
+        {' '}
+        <h1> Votação </h1>{' '}
+      </header>
+      {msg}
+      <Row className="votos">
+        <Form className="votos">
+          <Table responsive>
+            <thead>
+              <tr>
+                <th scope="col"> # </th>
+                <th scope="col"> Descrição </th>
+                <th scope="col"> Nome Candidato </th>
+                <th scope="col"> Voto </th>
+              </tr>
+            </thead>
+            <tbody>{votterlist}</tbody>
+          </Table>
+          <Row>
+            <Col></Col>
+            <Col>
+              <ToggleButton
+                className="mb-2"
+                id="toggle-check"
+                type="checkbox"
+                variant="outline-primary"
+                checked={checked}
+                value="1"
+                onChange={(e) => setChecked(e.currentTarget.checked)}
+              >
+                Voto Público
+              </ToggleButton>
+            </Col>
+            <Col>
+              <Button
+                variant="secondary"
+                className="button"
+                onClick={history.goBack}
+              >
+                Voltar atrás
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Row>
+      <Row className="rodape2">
+        <p>Votrónico+ | Vitor Costa 2021</p>
+      </Row>
+    </Container>
+  );
 }
 
 export default VotingOption;
